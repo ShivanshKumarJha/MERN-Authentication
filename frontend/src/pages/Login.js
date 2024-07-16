e79dac73c79d3f563b46ed9c19963b23ae812fb9
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2024-07-17 02:47:18 +0530

    add useSignup useLogin useLogout hooks

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { login, error, isLoading } = useLogin();
 
   async function handleSubmit(e) {
     e.preventDefault();
-    console.log(email, password);
+
+    await login(email, password);
   }
 
   return (
@@ -27,7 +30,8 @@ function Login() {
         onChange={e => setPassword(e.target.value)}
       />
 
-      <button>Log in</button>
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 }
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
+import { useSignup } from '../hooks/useSignup';
 
 function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { signup, isLoading, error } = useSignup();
 
   async function handleSubmit(e) {
     e.preventDefault();
-    console.log(email, password);
+
+    await signup(email, password);
   }
 
   return (
@@ -27,7 +30,8 @@ function Signup() {
         onChange={e => setPassword(e.target.value)}
       />
 
-      <button>Sign up</button>
+      <button disabled={isLoading}>Sign up</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 }
