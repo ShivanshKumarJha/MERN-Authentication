d10bf7c1a6a3506a0e99c95148a19e22479ebd96
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2024-07-17 02:38:54 +0530

    correct code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,7 +28,7 @@ const signupUser = async (req, res) => {
 
     const token = createToken(user._id);
 
-    res.status(200).json(email, token);
+    res.status(200).json({email, token});
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
